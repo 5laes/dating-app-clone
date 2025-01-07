a5aa2f5d8653e2a8d595ca94a7d84752b61319c2
5laes <92576622+5laes@users.noreply.github.com>
2025-01-07 21:29:48 +0100

    Added members edit and loading Members can now edit their profile Added a updateMemberDto for use when updating a member to only update trivial information 'not names or date of birth' Added a loading spinner to indicate loading Added fake loading time for development Added a guard that warns users that they have unsaved changes if they try to navigate away from the edit page when stuff is unsaved

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
@@ -30,6 +31,21 @@ namespace API.Controllers
         {
             var user = await _userRepository.GetMemberAsync(username);
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
