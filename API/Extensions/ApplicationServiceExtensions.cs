75eed255b22c79680da03c5930b84a517ec646ef
5laes <92576622+5laes@users.noreply.github.com>
2025-01-08 18:25:36 +0100

    Added photo editor using Cloudinary Users can now add add/delete photos and change their main photo Added ng2-file-upload package to handle uploading files from a user both drag and drop and select

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,8 +10,9 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config, bool isDevelopment)
         {
-            
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
